2a2a16f5c84206d582a9c590fe9112265927e76d
Henrietta <h_henrietta@outlook.com>
2023-01-09 14:09:19 +0100

    Put the availability checker function in its class

--- availabilityChecker/checker.py
+++ availabilityChecker/checker.py
@@ -1,18 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www2.hm.com/hu_hu/productpage.1044156002.html")
-button = driver.find_element(By.CLASS_NAME, "trigger-button")
-button.click()
 
-size_options = driver.find_elements(By.CSS_SELECTOR, ".option span")
+class AvailabilityChecker:
+    def __init__(self, articlenumber, size):
+        self.size = size
+        self.articlenumber = articlenumber
+        self.driver = webdriver.Chrome()
+        self.driver_init()
 
-for option in size_options:
-    value = option.text
-    classes = option.get_attribute("class")
-    if value == "32" and "oos" not in classes:
-        print('The selected size is available.')
+        self.button_clicker()
+        self.checker_print_to_console(self.checker())
+
+        self.driver.quit()
+
+    def driver_init(self):
+        self.driver.get(f"https://www2.hm.com/hu_hu/productpage.{self.articlenumber}.html")
+
+    def button_clicker(self):
+        button = self.driver.find_element(By.CLASS_NAME, "trigger-button")
+        button.click()
+
+    def checker(self):
+        size_options = self.driver.find_elements(By.CSS_SELECTOR, ".option span")
+        for option in size_options:
+            value = option.text
+            classes = option.get_attribute("class")
+            if value == self.size and "oos" not in classes:
+                return True
+        return False
+
+    def checker_print_to_console(self, available):
+        if available:
+            print(f'The selected size ({self.size}) is available.')
+        else:
+            print(f'The selected size ({self.size}) is not available.')
 
 
-driver.quit()
+check = AvailabilityChecker("1044156002", "32")
+check2 = AvailabilityChecker("1044156002", "38")
+check3 = AvailabilityChecker("1140505001", "XS")
